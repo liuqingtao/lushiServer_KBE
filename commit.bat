b14f1b4f610c47c52d4e52bcf9741d1cd906b14f
liuqingtao <lqtmail@163.com>
2018-06-26 16:20:01 +0800

    normal up

--- commit.bat
+++ commit.bat
@@ -1,13 +1,14 @@
-cd /c C:\Users\Public\Documents\workspace\kbe\KBEProject
+cd  C:\Users\Public\Documents\workspace\kbe\KBEProject
 git add .
 git commit -am "normal up"
 git push origin master
 
-cd /c C:\Users\Public\Documents\workspace\kbe\kbengine-kbengine-v1.0.0\kbengine\assets
+cd  C:\Users\Public\Documents\workspace\kbe\kbengine-kbengine-v1.0.0\kbengine\assets
 git add .
 git commit -am "normal up"
 git push origin master
 
-cd /c C:\Users\Public\Documents\workspace\kbe\KBEProject2
-git reset --hard
+cd  C:\Users\Public\Documents\workspace\kbe\KBEProject2
+git fetch --all
+git reset --hard origin master
 git pull origin master
